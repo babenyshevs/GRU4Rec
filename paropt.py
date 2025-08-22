1ef4ce65bdabc6dc16567051cf96d90006fd9e01
Stanislav Babenyshev <31929549+babenyshevs@users.noreply.github.com>
2025-08-22 11:15:19 +0200

    Refactor scripts into main entry points

--- paropt.py
+++ paropt.py
@@ -12,11 +12,9 @@ CONFIG_ENV_VAR = "GRU4REC_PAROPT_CONFIG"
 DEFAULT_CONFIG_PATH = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), "config", "paropt.json"
 )
-config_path = os.environ.get(CONFIG_ENV_VAR, DEFAULT_CONFIG_PATH)
-config = SimpleNamespace(**config_loader.load_paropt_config(config_path))
 
 
-def generate_command(optimized_param_str, measure, log_primary_metric=True):
+def generate_command(config, optimized_param_str, measure, log_primary_metric=True):
     """Create command to invoke run.py with a temporary config file."""
     param_parts = [config.fixed_parameters] if config.fixed_parameters else []
     if optimized_param_str:
@@ -44,8 +42,8 @@ def generate_command(optimized_param_str, measure, log_primary_metric=True):
     return command, tmp.name
 
 
-def run_once(optimized_param_str):
-    command, cfg_path = generate_command(optimized_param_str, config.measure, True)
+def run_once(config, optimized_param_str):
+    command, cfg_path = generate_command(config, optimized_param_str, config.measure, True)
     cmd = pexpect.spawnu(command, timeout=None, maxread=1)
     line = cmd.readline()
     val = None
@@ -112,38 +110,46 @@ class Parameter:
         return desc
 
 
-def objective(trial, par_space):
+def objective(trial, par_space, config):
     optimized_param_str = []
     for par in par_space:
         val = par(trial)
         optimized_param_str.append('{}={}'.format(par.name, val))
     optimized_param_str = ','.join(optimized_param_str)
-    val = run_once(optimized_param_str)
+    val = run_once(config, optimized_param_str)
     return val
 
 
-par_space = []
-with open(config.optuna_parameter_file, 'rt') as f:
-    print('-' * 80)
-    print('PARAMETER SPACE')
-    for line in f:
-        par = Parameter.fromjson(line)
-        print('\t' + str(par))
-        par_space.append(par)
-    print('-' * 80)
+def main():
+    config_path = os.environ.get(CONFIG_ENV_VAR, DEFAULT_CONFIG_PATH)
+    config = SimpleNamespace(**config_loader.load_paropt_config(config_path))
 
-study = optuna.create_study(direction='maximize')
-study.optimize(lambda trial: objective(trial, par_space), n_trials=config.ntrials)
+    par_space = []
+    with open(config.optuna_parameter_file, 'rt') as f:
+        print('-' * 80)
+        print('PARAMETER SPACE')
+        for line in f:
+            par = Parameter.fromjson(line)
+            print('\t' + str(par))
+            par_space.append(par)
+        print('-' * 80)
 
-print('Running final eval @{}:'.format(config.final_measure))
-optimized_param_str = ','.join(['{}={}'.format(k, v) for k, v in study.best_params.items()])
-command, cfg_path = generate_command(optimized_param_str, config.final_measure, False)
-cmd = pexpect.spawnu(command, timeout=None, maxread=1)
-line = cmd.readline()
-while line:
-    line = line.strip()
-    print(line)
+    study = optuna.create_study(direction='maximize')
+    study.optimize(lambda trial: objective(trial, par_space, config), n_trials=config.ntrials)
+
+    print('Running final eval @{}:'.format(config.final_measure))
+    optimized_param_str = ','.join(['{}={}'.format(k, v) for k, v in study.best_params.items()])
+    command, cfg_path = generate_command(config, optimized_param_str, config.final_measure, False)
+    cmd = pexpect.spawnu(command, timeout=None, maxread=1)
     line = cmd.readline()
-cmd.wait()
-os.remove(cfg_path)
+    while line:
+        line = line.strip()
+        print(line)
+        line = cmd.readline()
+    cmd.wait()
+    os.remove(cfg_path)
+
+
+if __name__ == "__main__":
+    main()
 
